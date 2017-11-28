64473a09cd88f9d565f91ee31a7a3a1a8b9fa6a3
colsarcol <git@cmawhinney.com>
2017-11-28 01:20:15 -0500

    Logic error with when the email was sending, it was calling the ask again function before it sent the email so it was stuck in a loop and would not send till yes or no was triggered

--- maybe.py
+++ maybe.py
@@ -6,9 +6,9 @@ emailContact = ""
 
 def maybe(emailContact):
 	os.system("clear")
-	print("\nMaybe? I like these odds, but umm lets go with a yes or no this time!")
-	ask.questionFun(emailContact)	
+	print("\nMaybe? I like these odds, but umm lets go with a yes or no this time!")	
 	confirmEmail.maybe(emailContact)
+	ask.questionFun(emailContact)	
 
 def main():
 	maybe(emailContact)
