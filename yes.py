046628c3bd38b3e55e8516b6d638ba653f014376
colsarcol <git@cmawhinney.com>
2017-11-30 21:15:12 -0500

    Added time function to ask what time is best to meet

--- yes.py
+++ yes.py
@@ -22,27 +22,71 @@ def yes(emailContact):
 	option = input("Option (1, 2, 3, 4 or 5): ")
 	option = str(option)
 	if(option == "1"):
-		confirmEmail.yes("Friday - " + str(friday), emailContact)
-		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+		dateStr = "Friday - " + str(friday)
+		time(emailContact, dateStr)
 	elif(option == "2"):
-		confirmEmail.yes("Saturday - " + str(sat), emailContact)
-		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+		dateStr = "Saturday - " + str(sat)
+		time(emailContact, dateStr)
 	elif(option == "3"):
-		confirmEmail.yes("Sunday - " + str(sun), emailContact)
-		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+		dateStr = "Sunday - " + str(sun)
+		time(emailContact, dateStr)
 	elif(option == "4"):
 		print("\nWhat date is good for you?")
 		yourDate = input("Enter Date: ")
-		confirmEmail.yes(str(yourDate), emailContact)
-		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+		dateStr = str(yourDate)
+		time(emailContact, dateStr)
 	elif(option == "5"):
 		exit()
 	else:
 		os.system("clear")
 		main()
 
+def time(emailContact, dateStr):
+	dateStr = dateStr
+	emailContact = emailContact
+	print("\nWhat time is best to meet on " + dateStr + "\n")
+	print("1) 5 pm")
+	print("2) 6 pm")
+	print("3) 7 pm")
+	print("4) 8 pm")
+	print("5) 9 pm")
+	print("6) 10 pm")
+	print("7) Exit")
+	optionTime = input("\nOption (1, 2, 3, 4, 5, 6 or 7): ")
+	optionTime = str(optionTime)
+	
+	if(optionTime == "1"):
+		timeVar = "5 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "2"):
+		timeVar = "6 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "3"):
+		timeVar = "7 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "4"):
+		timeVar = "8 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "5"):
+		timeVar = "9 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "6"):
+		timeVar = "10 PM"
+		confirmEmail.yes(dateStr, emailContact, timeVar)
+		print("\nAwesome, can't wait to see you! Check your " + emailContact + " email for confirmation.\n")
+	elif(optionTime == "7"):
+		exit()
+	else:
+		print("Invalid command, please restart the program")
+		exit()
+
 def main():
-	yes(emailContact, emailContact)
+	yes(emailContact)
 
 if __name__ == "__main__":
 	main()
