39c4aa7c6118ed088463f99b1b2edb1ee2fade0c
colsarcol <git@cmawhinney.com>
2017-11-30 20:28:11 -0500

    Removed the new line in the front of the error message

--- maybe.py
+++ maybe.py
@@ -6,7 +6,7 @@ emailContact = ""
 
 def maybe(emailContact):
 	os.system("clear")
-	print("\nMaybe? I like these odds, but umm lets go with a yes or no this time!")	
+	print("Maybe? I like these odds, but umm lets go with a yes or no this time!")	
 	confirmEmail.maybe(emailContact)
 	ask.questionFun(emailContact)	
 
